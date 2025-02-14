6f9a4008674dbe4502dbd778e7ea6f66f1cd9ac4
vanwtoanf <damtoan321@gmail.com>
2025-02-14 15:21:35 +0700

    .........

--- env_mag.sql
+++ env_mag.sql
@@ -1,12 +1,9 @@
-/*!999999\- enable the sandbox mode */ 
+/*!999999 enable the sandbox mode */ 
 -- MariaDB dump 10.19  Distrib 10.11.8-MariaDB, for debian-linux-gnu (x86_64)
 --
 -- Host: localhost    Database: env_mag
 -- ------------------------------------------------------
 -- Server version	10.11.8-MariaDB-0ubuntu0.24.04.1
-
-CREATE DATABASE IF NOT EXISTS env_mag;
-USE env_mag;
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
